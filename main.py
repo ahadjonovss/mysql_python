5bc71aa012b07e4ce1c91e5d4c3fc6cae53162d8
ahadjonovss <samandarahadjonov@gmail.com>
2023-07-18 12:28:12 +0500

    some

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import mysql.connector
 import my_sql_service as mySql
+import os
 
 mySqlService = mySql.MySqlService()
 
-
 print("TANLANG")
-print("1. Database ro'yxatini olish  2. Database qo'shish")
-firstDecision = int(input())
-if firstDecision == 1:
-    mySqlService.showDatabases()
+while True:
+    print("1. Database ro'yxatini olish  2. Database qo'shish  3. Chiqish")
+    firstDecision = int(input())
+    if firstDecision == 1:
+        mySqlService.showDatabases()
+
+
+    if firstDecision == 2:
+        mySqlService.createDatabase()
+    if firstDecision == 3:
+        break
+
+
 
--- my_sql_service.py
+++ my_sql_service.py
@@ -1,8 +1,11 @@
 import mysql.connector
+import time as t
+
 
 
 class MySqlService:
     def __init__(self):
+        self.allDatabases = None
         self.db = mysql.connector.connect(
             host="localhost",
             user='root',
@@ -12,5 +15,28 @@ class MySqlService:
 
     def showDatabases(self):
         self.cursor.execute("show databases;")
-        for i, v in enumerate(self.cursor.fetchall()):
-            print(f"{i+1} - {v[0]}")
+        self.allDatabases = self.cursor.fetchall()
+        for i, v in enumerate(self.allDatabases):
+            print(f"{i + 1} - {v[0]}")
+        self.selectDatabase()
+
+
+    def showTables(self):
+        self.cursor.execute("Show tables;")
+        print(self.cursor.fetchall())
+
+
+
+    def selectDatabase(self):
+        selectedDb = int(input("Kerakli database idsini yozing  "))
+        self.cursor.execute(f"Use {self.allDatabases[selectedDb-1][0]};")
+        self.showTables()
+
+    def createDatabase(self):
+        dbName = input("Database uchun nom kiriting: ")
+        print(f"{dbName} nomli database create qilinmoqda...")
+        t.sleep(3)
+        print("Oz qoldi...")
+        t.sleep(2)
+        self.cursor.execute(f"Create database if not exists {dbName}")
+        print("Database muvaffaqqiyatli ochildi")
